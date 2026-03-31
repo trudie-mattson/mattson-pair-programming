de0e631f1aeb5dbb65ace4962257e81792ec26d5
AJ Oven <aj_owen@mines.edu>
2026-03-31 13:36:46 -0600

    Add feedback and test function

--- pair_programming.py
+++ pair_programming.py
@@ -4,6 +4,7 @@
 #function
 def meters(feet, inches):
     if feet.isalpha() == True or inches.isalpha() == True: #check for alpha values
+        #this if statement doesnt work, it causes an error, I think the .isaplha() operator is not working.
         print('Please input a number for feet and inches')
         feet = input('feet: ')
         inches = input('inches: ')
@@ -12,5 +13,5 @@ def meters(feet, inches):
     feet_meters = float(feet) * 0.3048
     inches_meters = float(inches) * 0.0254
     value = feet_meters + inches_meters
-
+#other then the .isalpha() operator the code looks good.
     return value
